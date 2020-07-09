dc7730c60ebfd6d4a2bef021d45f528cb3569c97
Igor Eskin <eskinhd@gmail.com>
2020-07-09 15:29:13 -0400

    Input params displayed on submit

--- src/InputComponent.js
+++ src/InputComponent.js
@@ -1,21 +1,73 @@
 import React, { useState } from 'react';
 import './index.css';
 
-const InputComponent = () => {
+export const useInputParamsSettings = (props) => {
+  const [inputParams, setInputParams] = useState({
+    userId: "",
+    firstName: "",
+    lastName: ""
+  });
+  const setInputText = propertyWithValue => {
+    setInputParams({ ...inputParams, ...propertyWithValue });
+  };
+  return {
+    inputParamsSettings: {
+      setInputText,
+      inputParams
+    }
+  }
+}
+
+const InputComponent = (props) => {
+
+  const { inputParamsSettings: { setInputText, inputParams }} = useInputParamsSettings(props);
+
+  const setFormData = e => {
+    setInputText({ [e.target.name]: e.target.value });
+  }
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleClearClick = () => {
+    setSubmitted(false);
+    setInputText({ userId: "", firstName: "", lastName: ""})
+    console.log(inputParams)
+  }
+
   return (
-    <div>
+    <div className="main-container">
       <div>
-        <label >User Id</label>
-        <input name="userId" type="text" />
+        <div>
+          <label>User Id</label>
+          <input name="userId" type="text" onChange={setFormData} />
+        </div>
+        <div>
+          <label>First Name</label>
+          <input name="firstName" type="text" onChange={setFormData} />
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input name="lastName" type="text" onChange={setFormData} />
+        </div>
       </div>
-      <div>
-        <label >First Name</label>
-        <input name="firstName" type="text" />
-      </div>
-      <div>
-        <label >Last Name</label>
-        <input name="lastName" type="text" />
-      </div>
+
+      <button onClick={e => setSubmitted(true)}>Submit</button>
+      <button onClick={handleClearClick}>Clear</button>
+
+      {submitted && <div className="formOutput">
+        <div>
+          <label>UserId</label>
+          <span className="input">{inputParams.userId}</span>
+        </div>
+        <div>
+          <label>First Name</label>
+          <span className="input">{inputParams.firstName}</span>
+        </div>
+        <div>
+          <label>Last Name</label>
+          <span className="input">{inputParams.lastName}</span>
+        </div>
+      </div>}
     </div>
   )
 }
